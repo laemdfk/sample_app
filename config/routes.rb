d9f93d46200d36c4b95759490e56c0640a7faf38
laemdfk <kappa021618@icloud.com>
2022-08-04 13:54:31 +0000

    [Add]homes_controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # routing => " HTTPメソッド 'URL' => 'コントローラ#アクション' "の"
+  
+  #root toを使うときは、 root to: 'コントローラ#アクション'の形で。
+  
+  root to: 'homes#top'
+  get 'homes/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
