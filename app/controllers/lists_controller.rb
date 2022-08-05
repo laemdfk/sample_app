e42f0c00d772b7b185e9e66c411467ecb4a5bfe2
laemdfk <kappa021618@icloud.com>
2022-08-05 14:12:27 +0000

    [Add]create,new,index機能作成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,22 +1,33 @@
 class ListsController < ApplicationController
   def new
+    @list_new  = List.new
   end
-  
+
   def create
+    @list_new = List.new(list_params)
+    @list_new.save
+    redirect_to lists_path
   end
 
   def show
   end
 
   def index
+    @lists = List.all
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
+
+  private
+   def list_params
+     params.require(:list).permit(:title, :body)
+   end
+
 end
