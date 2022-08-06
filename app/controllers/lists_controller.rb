738b7092a996bd29132fe30eb4e0b9cc2cad9340
laemdfk <kappa021618@icloud.com>
2022-08-06 09:30:39 +0000

    [Add]バリデーション設定

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,8 +5,11 @@ class ListsController < ApplicationController
 
   def create
     @list_new = List.new(list_params)
-    @list_new.save
-    redirect_to lists_path
+    if @list_new.save
+     redirect_to lists_path
+    else
+      render :new
+    end
   end
 
   def show
